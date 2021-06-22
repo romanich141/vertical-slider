29a6f2fbfeb6b233c978981a9aa4eac15f671d71
Roma Ovcharenko <prog3@gerchikco.com>
2021-06-22 20:38:00 +0300

    + styles

---
+++ index.js
@@ -0,0 +1,11 @@
+const sliderImages = document.querySelector('.slider__images');
+const sliderWrapper = document.querySelector('.slider__wrapper');
+
+const height = sliderWrapper.clientHeight;
+sliderImages.style.top = `-${3 * 100}vh`;
+
+let activeImage = 0;
+
+
+
+console.log(sliderImages)
