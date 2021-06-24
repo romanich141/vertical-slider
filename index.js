0b59fa04be5cdc5666938ef02464dfd906adb8d3
Roma Ovcharenko <prog3@gerchikco.com>
2021-06-24 18:37:09 +0300

    changes script; changes styles

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+const btnUp = document.querySelector('.button--up');
+const btnDown = document.querySelector('.button--down');
 const sliderImages = document.querySelector('.slider__images');
+const sidebar = document.querySelector('.slider__sidebar');
 const sliderWrapper = document.querySelector('.slider__wrapper');
-
-const height = sliderWrapper.clientHeight;
-sliderImages.style.top = `-${3 * 100}vh`;
+const countImages = sliderImages.querySelectorAll("div.image__item").length;
 
 let activeImage = 0;
 
+const height = sliderWrapper.clientHeight;
+// sliderImages.style.top = `-${activeImage * 100}vh`;
 
+const slideImage = direction => {
+  switch (direction) {
+    case "up":
+      activeImage++;
+      if (activeImage === countImages) activeImage = 0;
+      break;
+      
+    case "down":
+      activeImage--;
+      if (activeImage < 0) activeImage = countImages - 1;
+      break;
+    }
 
-console.log(sliderImages)
+    sliderImages.style.transform = `translateY(-${activeImage * height}px)`;  
+    // sidebar.style.transform = `translateY(${activeImage * height}px)`;
+}
+    
+btnUp.addEventListener('click', () => slideImage("up"));
+btnDown.addEventListener('click', () => slideImage("down"));
+
